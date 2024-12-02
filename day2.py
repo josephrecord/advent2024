0f032c48af586d24ace8cafb469ef48d341608dd
josephrecord <joseph.record@gmail.com>
2024-12-02 18:47:10 -0500

    remove print statements

--- day2.py
+++ day2.py
@@ -3,25 +3,17 @@ import operator
 from itertools import pairwise, starmap, filterfalse
 
 def my_diff(report: list[int]) -> list[int]:
-    return starmap(operator.sub, pairwise(report))
+    return list(starmap(operator.sub, pairwise(report)))
 
 
 num_safe = 0
 
 with open("input2.txt") as f:
     for line in f:
-        l = [int(x) for x in line.split()]
-        diff = list(my_diff(l))
-        print("list")
-        print(l)
-        print(list(my_diff(l)))
-
-        if not list(filterfalse(lambda x: 0<x<4, diff)):
-            print("decreasing")
+        diffs = my_diff([int(x) for x in line.split()])
+        if not list(filterfalse(lambda x: 0<x<4, diffs)):
             num_safe += 1
-
-        if not list(filterfalse(lambda x: -4<x<0, diff)):
-            print("increasing")
+        if not list(filterfalse(lambda x: -4<x<0, diffs)):
             num_safe += 1
 
 
