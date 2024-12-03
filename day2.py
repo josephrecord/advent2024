93c0521d76be7f7d629cd9888e8337e2163d6c83
josephrecord <joseph.record@gmail.com>
2024-12-03 07:33:38 -0500

    check inc and dec separately

--- day2.py
+++ day2.py
@@ -7,17 +7,15 @@ def my_diff(report: list[int]) -> list[int]:
 
 
 
-def check(differences):
-    increasing = None
-    for elem in differences:
-        if increasing is None:
-            if elem < 0:
-                increasing = True
-            elif elem > 0:
-                increasing = False
-            else:
-                pass
-        
+def check_inc(diffs):
+    if not list(filterfalse(lambda x: -4<x<0, diffs)):
+        return True
+    return False
+
+def check_dec(diffs):
+    if not list(filterfalse(lambda x: 0<x<4, diffs)):
+        return True
+    return False     
 
 
 
@@ -27,25 +25,11 @@ with open("input2.txt") as f:
     for line in f:
         diffs = my_diff([int(x) for x in line.split()])
 
-        filtered = [x for x in diffs if -4 < x < 4]
-
-        print(line)
-        print(diffs)
-        print(filtered)
-
-
-        # decreasing_elems = list(filterfalse(lambda x: 0<x<4, diffs))
-        # if len(decreasing_elems) == 0 or len(decreasing_elems) == 1:
-        #     if not [x for x in decreasing_elems if x < 4]:
-        #         num_safe += 1
-        #         print(line)
-        #         print(diffs)
-        # increasing_elems = list(filterfalse(lambda x: -4<x<0, diffs))
-        # if len(increasing_elems) == 0 or len(increasing_elems) == 1:
-        #     if not [x for x in increasing_elems if x < -4]:
-        #         num_safe += 1
-        #         print(line)
-        #         print(diffs)
+        if check_inc(diffs):
+            num_safe += 1
+        
+        if check_dec(diffs):
+            num_safe += 1
 
 
 print(num_safe)
